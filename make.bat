ab05a2ac54e1785b92fdf3efd06ff1fd91566595
abcdefg30 <alphabet@streber24.de>
2015-04-25 16:55:30 +0200

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,5 +1,8 @@
 @echo off
-echo "cl" to Clean Mod folder, "cp" to Copy the Mod, "cc" to Clean folder and Copy the Mod
+echo "cl" to Clean Mod folder
+echo "cp" to Copy the Mod
+echo "cc" to Clean folder and Copy the Mod
+echo "cpback" to Copy the Mod from "openra\mods\ra2" to "ra2"
 set /p cmd=Command:
  
 echo "You chose %cmd%.."
@@ -9,27 +12,34 @@ goto :do_%cmd%
 :do_cl
 echo Cleaning...
 rd /s /q "openra\mods\ra2\"
-echo:
-echo:done.
+echo.
+echo Done.
 goto end
 
 :do_cp
-echo:Copying...
-xcopy ra2 openra\mods\ra2\ /e /Y
-echo:
-echo:done.
+echo Copying...
+xcopy ra2 "openra\mods\ra2\" /e /Y
+echo.
+echo Done.
 goto end
 
 :do_cc
 echo Cleaning....
 rd /s /q "openra\mods\ra2\"
-echo:
-echo:done.
-echo:
-echo:Copying...
-echo:
-xcopy ra2 openra\mods\ra2\ /e /Y
-echo:
-echo:done.
+echo.
+echo Done.
+echo Copying...
+xcopy ra2 "openra\mods\ra2\" /e /Y
+echo.
+echo Done.
 goto end
+
+:do_cpback
+echo Copying...
+xcopy "openra\mods\ra2" ra2 /e /Y
+echo.
+echo Done.
+goto end
+
 :end
+pause
